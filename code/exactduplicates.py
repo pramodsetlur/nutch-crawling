389ecb3a74eed8ae1b5c32395f490f008ead25ad
sanjay <sanjayj@usc.edu>
2015-10-15 02:34:37 -0700

    adding conf files

--- code/exactduplicates.py
+++ code/exactduplicates.py
@@ -1,70 +1,51 @@
 import md5
-import tika
-tika.initVM()
-from tika import parser
+
 
 URLlist = []
 URLdict = {}
-Dup_URL_list = {}
-def read_dump():
+Dup_URL_list = []	
+
+# Reads URLlist.txt and stores the image URLs in a list
+def read_URLlist():	
 	global URLlist
-	fr = open('dump','r');
+	fr = open('URLlist.txt','r')
 	lines = fr.readlines()
 	for i in range(0,len(lines)):
-		sublist = []
-		if lines[i].startswith("URL"):
-			sublist = lines[i].split(":: ")
-			if len(sublist) > 1:
-				if sublist[1].endswith("jpg\n") or sublist[1].endswith("JPG\n") or sublist[1].endswith("png\n") or sublist[1].endswith("PNG\n") or sublist[1].endswith("gif\n") or sublist[1].endswith("GIF\n") or sublist[1].endswith("bmp\n") or sublist[1].endswith("BMP\n"):
-					URLlist.append(sublist[1].strip('\n'))
-			#URLlist.append(sublist[0])
+		URLlist.append(lines[i])
 	fr.close()
-	
-	fw = open('URLlist.txt','w')
-	for i in range(0,len(URLlist)):
-		fw.write(URLlist[i]+'\n')
-	fw.close()
 
-def extract_metadata():
-	fw = open('parsetext','w')
-	for i in range(0,20):
-		parsed = parser.from_file(URLlist[i])
-		for k,v in parsed["metadata"].iteritems():
-			fw.write(k)	
-			try:
-				fw.write(str(v))
-			except UnicodeEncodeError:	
-				fw.write(str(v.encode('utf-8')))
-		fw.write('\n')
-	fw.close()
-
+# Finds exact duplicate image URLs by using md5 python lib to calculate hash values 
 def find_duplicateURLs():
 	global URLdict, Dup_URL_list, URLlist
-	fr = open('parsetext','r')
+	fr = open('image_metadata.txt','r')
 	lines = fr.readlines()
-	met = ""
+	
 	for i in range(0,len(lines)):
+		met = ""
 		for j in range(0,len(lines[i]),5):
 			met+=lines[i][j]
 		hashval = md5.new(met).hexdigest()
-		if hashval not in URLdict.values():
-			URLdict[URLlist[i]]	= hashval
-		else:
-			for url,val in URLdict:
-				if val == hashval:
+		if str(hashval) in URLdict.values():
+			Dup_URL_list.append(URLlist[i])
+			for url,val in URLdict.iteritems():
+				if val == hashval and url not in Dup_URL_list:
 					Dup_URL_list.append(url)
-					Dup_URL_list.append(URLlist[i])
+		else:
+			URLdict[str(URLlist[i])] = str(hashval)
 	
+	fr.close()
+
 	if len(Dup_URL_list) > 0:
+		print "The exact duplicate image URLs are:"
 		for i in range(0,len(Dup_URL_list)):
 			print Dup_URL_list[i]
 	else:
-		print "No exact duplicate URLs"
-			  
-def start():
-	read_dump()
-	extract_metadata()
+		print "No exact Duplicate image URLs"
+
+def main():
+	read_URLlist()
 	find_duplicateURLs()
 	
 
-start()
+if __name__ == '__main__':
+	main()
